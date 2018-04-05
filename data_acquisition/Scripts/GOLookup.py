ecb41a21a8570f62bbcefdd2148e8290e637cd35
Gage Black <GBlack@GBlack-Pro.app.byu.edu>
2018-04-05 12:09:36 -0600

    Merge with upstream

--- data_acquisition/Scripts/GOLookup.py
+++ data_acquisition/Scripts/GOLookup.py
@@ -4,39 +4,63 @@ import requests
 import argparse
 
 
-p = argparse.ArgumentParser()
-p.add_argument("go_id", help = "Enter the GO_ID you would like to lookup")
-p.add_argument("-r", "--human_readable", action = "store_true", help = "Specify if you would like stuff printed in user friendly fashion")
-args = p.parse_args()
 
-pre_query = "https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/"
-query_id = args.go_id.replace(":", "%3A")
-full_query = pre_query + query_id + "/complete"
+def RetrieveGOData(goid, readable):
+	pre_query = "https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/"
+	query_id = goid.replace(":", "%3A")
+	full_query = pre_query + query_id + "/complete"
 
-r = requests.get(full_query, headers = {"Accept" : "application/json"})
-if not r.ok:
-	r.raise_for_status()
-	sys.exit()
+	r = requests.get(full_query, headers = {"Accept" : "application/json"})
+	if not r.ok:
+		r.raise_for_status()
+		sys.exit()
 
-if args.human_readable:
-	import json
-	parsed_json = json.loads(r.text)
-	id = args.go_id
-	name =  parsed_json["results"][0]["name"]
-	definition = parsed_json["results"][0]["definition"]["text"]
-	print ("GO ID:\n" ,'\t', id)
-	print ("Name:\n", '\t', name)
-	print ("Definition:\n", '\t', definition)
-	try:
-		synonyms = parsed_json["results"][0]["synonyms"]
-	except:
-		synonyms = None
-	if synonyms:
-		print ("Synonyms:")
-		for e in synonyms:
-			print ('\t', e["name"])
-		
-	#should be parsed_json["results"][0]["name"]
-else:
-	print(r.text)
+	if readable:
+		import json
+		parsed_json = json.loads(r.text)
+		id = args.go_id
+		name =  parsed_json["results"][0]["name"]
+		definition = parsed_json["results"][0]["definition"]["text"]
+		print ("GO ID:\n" ,'\t', id)
+		print ("Name:\n", '\t', name)
+		print ("Definition:\n", '\t', definition)
+		try:
+			synonyms = parsed_json["results"][0]["synonyms"]
+		except:
+			synonyms = None
+		if synonyms:
+			print ("Synonyms:")
+			for e in synonyms:
+				print ('\t', e["name"])
+			
+		#should be parsed_json["results"][0]["name"]
+	else:
+		print(r.text)
 
+
+if __name__ == "__main__":
+
+	p = argparse.ArgumentParser()
+	p.add_argument("go_id", nargs='*', help = "Enter the GO_ID(s) you would like to lookup. A list of space-separated terms is also acceptable")
+	p.add_argument("-f", "--go_id_file", help = "A file containing a list of newline separated GO_terms to look up")
+	p.add_argument("-r", "--human_readable", action = "store_true", help = "Specify if you would like stuff printed in user friendly fashion")
+	args = p.parse_args()
+
+	if len(args.go_id) == 0 and args.go_id_file == None:
+		print ("Please specify either valid GO term(s) or a file listing GO terms")
+		print ("Program will terminate")
+		sys.exit()
+	
+	if args.go_id:
+		for goid in args.go_id:
+			RetrieveGOData(goid, args.human_readable)
+			print()
+	if args.go_id_file:
+		with open(args.go_id_file, "r") as go_in:
+			for line in go_in:
+				if "GO" in line:
+					RetrieveGOData(line.strip(), args.human_readable)
+					print()
+				else:
+					print("File in has invalid terms. Please try again")
+					sys.exit()
