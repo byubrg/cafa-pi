a9a37d3b6c066d081ed5f5ecd3bd4795dc178f25
Erica Suh <ekjsuh@gmail.com>
2018-04-10 19:14:09 -0600

    Works but super slow

--- ml/logreg.py
+++ ml/logreg.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from .embeddings import load_data_padded, get_batch, shuffle_unison
+from tensorflow.contrib.layers import flatten
 
 # Set random seed
 np.random.seed(0)
@@ -17,12 +18,12 @@ labels_test = labels[split_point:]
 # Parameters
 learning_rate = 0.01
 training_epochs = 1000
-batch_size = 100
+batch_size = 25
 display_step = 100
 
 # Graph input
-x_ = tf.placeholder(tf.float32, [None, 10000]) # seq length (500) x num aminos (20)
-x = tf.reshape(x_, [None, 500, 20])
+x_ = tf.placeholder(tf.float32, [None, 500, 20]) # seq length (500) x num aminos (20)
+x = flatten(x_)
 y = tf.placeholder(tf.float32, [None, 3]) # targets: cat, dad, both
 
 # Set model weights
@@ -30,7 +31,7 @@ W = tf.Variable(tf.random_normal([10000, 3], stddev=0.1))
 b = tf.Variable(tf.zeros([3]))
 
 # Construct model
-z = tf.nn.softmax(tf.matmul(x, W2) + b) # Softmax
+z = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
     
 # Define loss and optimizer
 #cost = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1))
@@ -52,9 +53,10 @@ with tf.Session() as sess:
         # Loop over all batches
         for i in range(1001):
             batch_inputs, batch_targets = get_batch(data_train, labels_train)
+        
             # Run optimization op (backprop) and cost op (to get loss value)
             _, l = sess.run([optimizer, cross_entropy], feed_dict={
-                x: batch_inputs,
+                x_: batch_inputs,
                 y: batch_targets,
             })
         #Display logs per epoch step
